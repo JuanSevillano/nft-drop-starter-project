06212a9ed3731108acef5b8265176e3fd9a86384
JuanSevillano <sevi0_6@hotmail.com>
2021-12-04 12:15:38 +0100

    :rocket: app connected to phantom wallet

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -6,13 +6,79 @@ import twitterLogo from './assets/twitter-logo.svg';
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+
+
 const App = () => {
+
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkWalletConnection = async () => {
+
+    try {
+
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found')
+
+
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(`Connected with public key ${response.publicKey.toString()}`);
+          setWalletAddress(response.publicKey.toString());
+
+        } else {
+          console.log('solana object not found get a phantom wallet 👻');
+        }
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+
+  const connectWallet = async () => {
+
+    const { solana } = window;
+
+    if (solana) {
+
+      const response = await solana.connect();
+      const pbkey = response.publicKey.toString();
+      console.log(`Connected with PBK: ${pbkey}`);
+
+      setWalletAddress(pbkey);
+    }
+
+  }
+
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+
+    const onLoad = async () => await checkWalletConnection();
+
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad)
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
+          <p className="header">🍭 Sevi's First Drop #BuildSpace</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
